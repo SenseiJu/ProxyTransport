aa64dc685e624dad0eaf98691440619cd2dbc822
SenseiJu <chathataranjit@yahoo.co.uk>
2026-07-15 19:52:54 +0100

    Refactor ProxyTransportCompressionCodec to remove unused prefixed variable and simplify encoding logic

--- src/main/java/org/nethergames/proxytransport/compression/ProxyTransportCompressionCodec.java
+++ src/main/java/org/nethergames/proxytransport/compression/ProxyTransportCompressionCodec.java
@@ -2,7 +2,6 @@ package org.nethergames.proxytransport.compression;
 
 import dev.waterdog.waterdogpe.network.connection.codec.compression.ProxiedCompressionCodec;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.CompositeByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import org.cloudburstmc.protocol.bedrock.data.CompressionAlgorithm;
 import org.cloudburstmc.protocol.bedrock.data.PacketCompressionAlgorithm;
@@ -13,25 +12,16 @@ import org.cloudburstmc.protocol.bedrock.netty.codec.compression.CompressionStra
 import java.util.List;
 
 public class ProxyTransportCompressionCodec extends ProxiedCompressionCodec {
-    private final boolean prefixed;
     private final ZstdCompression zstdCompression = new ZstdCompression();
 
     public ProxyTransportCompressionCodec(CompressionStrategy strategy, boolean prefixed) {
         super(strategy, prefixed);
-        this.prefixed = prefixed;
     }
 
     protected void encode(ChannelHandlerContext ctx, BedrockBatchWrapper msg, List<Object> out) throws Exception {
         if (msg.getCompressed() == null && msg.getUncompressed() == null) {
             throw new IllegalStateException("Batch was not encoded before");
         } else if (msg.getCompressed() != null && !msg.isModified()) { // already compressed
-            if (!this.prefixed) { // we need to prefix the compressed data
-                CompositeByteBuf buf = ctx.alloc().compositeDirectBuffer(2);
-                buf.addComponent(true, ctx.alloc().ioBuffer(1).writeByte(getCompressionHeader(msg.getAlgorithm())));
-                buf.addComponent(true, msg.getCompressed().retainedSlice());
-                msg.setCompressed(buf, msg.getAlgorithm());
-            }
-
             this.onPassedThrough(ctx, msg);
             out.add(msg.retain());
         } else {
